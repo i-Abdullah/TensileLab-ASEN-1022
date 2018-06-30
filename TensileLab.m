86f97578a89c156cf83b1fd60b4508644cc9cdce
Abdullah <iAbdullah.AlAmeri@gmail.com>
2018-06-30 10:09:34 -0600

    Added curve fitting sessions in matlab, the code have the correct graphs now

--- TensileLab.m
+++ TensileLab.m
@@ -124,98 +124,144 @@ close all
 
 
 subplot(1,2,1)
-scatter(Strain_SA_E,Stress_SA_E,2,'k')
+scatter(Strain_SA_E,Stress_SA_E,2,'k');
 hold on
-scatter(Strain_RG_E,Stress_RG_E,2,'r')
+scatter(Strain_RG_E,Stress_RG_E,2,'r');
 
 grid
-xlabel('Strain (uniteless)')
-ylabel('Stress (MPa)')
-title ('Stress Vs Strain - Sample E')
-legend('Sam and Abdulla','Raymie and Gera')
+xlabel('Strain (uniteless)');
+ylabel('Stress (MPa)');
+title ('Stress Vs Strain - Sample E');
+legend('Sam and Abdulla','Raymie and Gera');
 
 subplot(1,2,2)
 
-scatter(Strain_SA_C,Stress_SA_C,4,'k')
+scatter(Strain_SA_C,Stress_SA_C,4,'k');
 hold on
-scatter(Strain_RG_C,Stress_RG_C,2,'r')
-legend('Sam and Abdulla','Raymie and Gera')
+scatter(Strain_RG_C,Stress_RG_C,2,'r');
+legend('Sam and Abdulla','Raymie and Gera');
 grid
-xlabel('Strain (uniteless)')
-ylabel('Stress (MPa)')
-title ('Stress Vs Strain - Sample C')
+xlabel('Strain (uniteless)');
+ylabel('Stress (MPa)');
+title ('Stress Vs Strain - Sample C');
 
 hold off
 
 
-%% Detrmine the linear region of the data
+%% Analyzing: Y.S,Youngs Mouduls, T.S, Fracture point.
+
+
+%Tensile Strength is the max stress:
+
+TS_SA_E = max(Stress_SA_E);
+TS_SA_C = max(Stress_SA_C);
+TS_RG_E = max(Stress_RG_E);
+TS_RG_C = max(Stress_RG_C);
 
 
 %We will determine the best fit for the linear region of the data using the
 %curve fitting tool in MATLAB, we will exclude the non-linear regions by
-%the ruler tool.
+%the ruler tool in the curve fitting app.
 
-%We will store each curve fitting session in the main file, with the
-%function that the Curve fitting app can generate so we can call them
-%within this code just to reduce computation time .
+%We will store each curve fitting session in the main file, we will
+%manually extract the polynomial coefficients.
 
-%The functions will be named createFit
-
-%The first output of the function will create for us our polynomial
-%coefficients, we will just use the gradient for the first degree
-%ploynomial to represent Young's modulus
+%each young's equation represent the best fit line:
 
 
-syms x
+% BEST LINE EQUATIONS;
 
-Youngs_SA_E = Young_SA_E*x ;
+Youngs_SA_E = @(x) 8.633e+07 *(x - 0.002E-5) +  41.31 ;
+Youngs_SA_C = @(x) 3.411e+08 *(x - 0.002E-5) + -12.96 ;
+Youngs_RG_E = @(x) 1.089e+08 *(x - 0.002E-5) + -1.96;
+Youngs_RG_C = @(x) 4.075e+08 *(x - 0.002E-5) + -14.91
 
-hold all 
+
+% Determine the Y.S (where the offeseted Youngs moudule by 0.2% intersect
+% the data point we have): we will use a for loop to determine the closest
+% values of our data to our best fit, and interpolate.
 
 
-scatter(Strain_SA_E,Stress_SA_E,2, 'k' )
+
+
+
+
+%% PLOTS :
+
+
+
+% FIRST PLOT
+
+subplot(2,2,1)
+scatter(Strain_SA_E,Stress_SA_E,2,'k');
+hold on
+linefit1 = ezplot(Youngs_SA_E,[ min(Strain_SA_E) max(Strain_SA_E) min(Stress_SA_E) max(Stress_SA_E)]);
+
+set(linefit1,'LineWidth',2);
+
+grid minor
+xlabel('Strain (uniteless)');
+ylabel('Stress (MPa)');
+title ('Stress Vs Strain, Sample E, SA ');
+legend('Sam and Abdulla','Youngs mouduls');
+xlim([0 9e-5]);
+ylim([0 400]);
+
+%_______________________________
+
+% SECOND PLOT
+
+subplot(2,2,2)
+
+scatter(Strain_RG_E,Stress_RG_E,2,'k');
 
 hold on
 
-linefit1 = ezplot(Youngs_SA_E,[ min(Strain_SA_E) - 0.001 max(Strain_SA_E) - 0.001 min(Stress_SA_E) - 0.0001 max(Stress_SA_E) - 0.0001 ])
+linefit2 = ezplot(Youngs_RG_E,[ min(Strain_RG_E) max(Strain_RG_E) min(Stress_RG_E) max(Stress_RG_E)]);
+set(linefit2,'LineWidth',2);
 
-
-hold off 
-
-
-
-%set(linefit1,'LineWidth',2)
-
-grid
 grid minor
-xlabel('Strain (uniteless)')
+xlabel('Strain (uniteless)');
+ylabel('Stress (MPa)');
+title ('Stress Vs Strain, Sample E, RG ');
+legend('Raymie and Gera','Youngs mouduls');
+xlim([0 6e-5]);
+ylim([0 400]);
+
+
+subplot(2,2,3)
+
+scatter(Strain_SA_C,Stress_SA_C,4,'k');
+hold on
+linefit3 = ezplot(Youngs_SA_C,[ min(Strain_SA_C) max(Strain_SA_C) min(Stress_SA_C) max(Stress_SA_C)]);
+set(linefit3,'LineWidth',2);
+
+grid minor
+xlabel('Strain (uniteless)');
 ylabel('Stress (MPa)')
-title ('Stress Vs Strain - Sample E')
-%legend('Sam and Abdulla','Raymie and Gera')
+title ('Stress Vs Strain, Sample C, SA ');
+legend('Sam and Abdulla','Youngs mouduls');
+xlim([0 0.3e-5]);
+ylim([0 170]);
 
 
-%% Analyze: Young's mouduls, ultimate tensile strength, and breaking point.
+subplot(2,2,4)
 
-%Caclulculate the youngs moudules based on the linear data by estimating
-%the linar best fit
+scatter(Strain_RG_C,Stress_RG_C,2,'k');
+hold on
+linefit4 = ezplot(Youngs_RG_C,[ min(Strain_RG_C) max(Strain_RG_C) min(Stress_RG_C) max(Stress_RG_C)]);
+set(linefit4,'LineWidth',2);
 
+legend('Raymie and Gera','Youngs mouduls');
+grid minor
+xlabel('Strain (uniteless)');
+ylabel('Stress (MPa)');
+title ('Stress Vs Strain, Sample C, RG');
+xlim([0 0.3e-5]);
+ylim([0 210]);
 
-% Sam and Abdulla Sample E Linear data are roughlt between 0 to 275 Mpa, which
-% corressponds to the indices from 0 to 220
-
-SA_E_Liner = [ Strain_SA_E(1:220) , Stress_SA_E(1:220) ];
-
-X_SA_E = SA_E_Liner(:,1);
-Y_SA_E = SA_E_Liner(:,2);
-
-p = polyfit(SA_E_Liner(:,1),SA_E_Liner(:,2),1);
-
-% Sam and Abdulla Sample E Linear data are roughlt between 0 to 275 Mpa, which
-% corressponds to the indices from 0 to 220
-
-
-
+hold off
 
 
 
-
+%% PRINTING OUT THE DATA:
